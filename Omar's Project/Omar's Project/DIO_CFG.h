bc3e464fca4c86c7c94d685523f6968ee43965ac
omaralaa28898 <70954844+omaralaa28898@users.noreply.github.com>
2020-12-13 18:04:38 +0200

    Microcontroller (Slave)

---
+++ Omar's Project/Omar's Project/DIO_CFG.h
@@ -0,0 +1,25 @@
+﻿/*
+ * DIO_CFG.h
+ *
+ * Created: 11/12/2020 10:41:22 م
+ *  Author: GASSER
+ */ 
+
+
+#ifndef DIO_CFG_H_
+#define DIO_CFG_H_
+
+
+#include "DIO.h"
+
+typedef struct {
+	DIO_DirTypes PinDir;
+	STD_LevelTypes PinLevel;
+}Dio_PinCFG;
+
+#define PINCOUNT 32
+
+void DIO_Init(void);
+
+
+#endif /* DIO_CFG_H_ */
---
+++ Omar's Project/Omar's Project/main.c
@@ -0,0 +1,30 @@
+/*
+ * Omar's Project.c
+ *
+ * Created: 11/12/2020 10:37:18 م
+ * Author : GASSER
+ */ 
+
+
+#include <avr/io.h>
+#include "avr/interrupt.h"
+#include "STD_types.h"
+#include "DIO.h"
+#include "LCD.h"
+
+
+int main(void)
+{
+	DIO_Init();
+	SPI_Init('S');
+	LCD_Init();
+	LCD_clr();
+	
+	LCD_cmd(0x85);
+	LCD_StringPos("Welcome to",1,2);
+	LCD_StringPos("Omar.A Project",2,2);
+	while (1)	{				if( SPI_Receive() == 0x11 )		{			DIO_write(DIO_channelC6,STD_high);		}						if( SPI_Receive() == 0x22 )		{			DIO_write(DIO_channelC6,STD_low);		}					}
+
+
+	
+}
